9750a3bae0c7f7683ce70744cf63ba7dd760d585
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-19 18:53:41 +0300

    added test_add_ingredient_ingredient_added_successfully

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,5 +1,9 @@
+import pytest
+
+import ingredient_types
 from burger import Burger
 from bun import Bun
+from ingredient import Ingredient
 
 
 class TestBurger:
@@ -7,9 +11,21 @@ class TestBurger:
         my_bun = Bun("булочка с кунжутом", 100)
         my_burger = Burger()
         my_burger.set_buns(my_bun)
-        assert my_burger.get_price() == 200
+        expected_result = my_burger.bun.get_name()
+        assert expected_result == "булочка с кунжутом"
 
-    def test_add_ingredient_added_successfully(self):
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        [ingredient_types.INGREDIENT_TYPE_SAUCE, 'garlic', 8.5],
+        [ingredient_types.INGREDIENT_TYPE_FILLING, 'bacon', 25]
+    ])
+    def test_add_ingredient_ingredient_added_successfully(self, ingredient_type, name, price):
+        my_ingredient = Ingredient(ingredient_type, name, price)
+        my_burger = Burger()
+        my_burger.add_ingredient(my_ingredient)
+        assert len(my_burger.ingredients) == 1
 
-
-
+    def test_remove_ingredient_ingredient_removed_successfully(self, ingredient_type, name, price):
+        my_ingredient = Ingredient(ingredient_type, name, price)
+        my_burger = Burger()
+        my_burger.add_ingredient(my_ingredient)
+        assert len(my_burger.ingredients) == 1
