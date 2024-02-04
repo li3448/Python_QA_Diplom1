b37a2a7363326fbafb2bd7f526808b6a32c3aad3
maxim.salov <maxim.salov@aliexpress.ru>
2024-02-04 13:20:12 +0300

    second commit after

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -4,25 +4,27 @@ from ..bun import Bun
 
 
 class TestBun:
-    @pytest.mark.parametrize('name', ['bun'])
+    @pytest.mark.parametrize('name', [('bun'), ('bigbun')])
     def test_init_name(self, name):
         bun = Bun(name, None)
         assert bun.get_name() == name
 
-    @pytest.mark.parametrize('price', [10])
+    @pytest.mark.parametrize('price', [(10), (1.5)])
     def test_init_price(self, price):
         bun = Bun(None, price)
         assert bun.get_price() == price
 
-    @pytest.mark.parametrize('name, price', [('bun', 10)])
+    @pytest.mark.parametrize('name, price', [('bun', 10), ('bigbun', 1.5)])
     def test_check_type_name(self, name, price):
         bun = Bun(name, price)
         assert type(bun.get_name()) is str
+        assert bun.get_name() is name
 
-    @pytest.mark.parametrize('name, price', [('bigbun', 1.5)])
+    @pytest.mark.parametrize('name, price', [('bigbun', 1.5), ('bun', 10.0)])
     def test_check_type_price(self, name, price):
         bun = Bun(name, price)
         assert type(bun.get_price()) is float
+        assert bun.get_price() == price
 
 
 class TestNegativeBun:
