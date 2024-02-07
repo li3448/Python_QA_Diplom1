bd2077ff5b9057cea8bd8ae8fe52ef54e631db28
Polina <polinaantonovna@yandex.com>
2024-02-07 20:13:10 +0300

    Added changes according reviewer comments

--- tests/bun_test.py
+++ tests/bun_test.py
@@ -6,16 +6,23 @@ import pytest
 class TestBun:
 
     @pytest.mark.parametrize('bun_name',
-                             [TestData.BUN_NAME_VALID_BLACK, TestData.BUN_NAME_VALID_WHITE, TestData.BUN_NAME_VALID_RED,
-                              TestData.BUN_NAME_WITH_SYMBOLS, TestData.BUN_NAME_EMPTY])
+                             [TestData.BUN_NAME_VALID_BLACK, TestData.BUN_NAME_VALID_WHITE, TestData.BUN_NAME_VALID_RED])
     def test_get_name_successful(self, bun_name):
         bun = Bun(bun_name, TestData.BUN_PRICE_VALID)
-        bun.get_name()
         assert bun.get_name() == bun_name
 
+    @pytest.mark.parametrize('bun_name',
+                             [TestData.BUN_NAME_WITH_SYMBOLS, TestData.BUN_NAME_EMPTY])
+    def test_get_name_unsuccessful(self, bun_name):
+        bun = Bun(bun_name, TestData.BUN_PRICE_VALID)
+        assert bun.get_name() != bun_name
+
+    def test_get_price_unsuccessful(self):
+        bun = Bun(TestData.BUN_NAME_VALID_WHITE, TestData.BUN_PRICE_LESS_THAN_ZERO)
+        assert bun.get_price() != TestData.BUN_PRICE_LESS_THAN_ZERO
+
     @pytest.mark.parametrize('bun_price',
-                             [TestData.BUN_PRICE_VALID, TestData.BUN_PRICE_FLOAT, TestData.BUN_PRICE_LESS_THAN_ZERO])
+                             [TestData.BUN_PRICE_VALID, TestData.BUN_PRICE_FLOAT])
     def test_get_price_successful(self, bun_price):
         bun = Bun(TestData.BUN_NAME_VALID_WHITE, bun_price)
-        bun.get_price()
         assert bun.get_price() == bun_price
--- tests/ingridient_test.py
+++ tests/ingridient_test.py
@@ -7,23 +7,22 @@ import pytest
 class TestIngredient:
 
     @pytest.mark.parametrize('price',
-                             [TestData.BUN_PRICE_VALID, TestData.BUN_PRICE_FLOAT, TestData.BUN_PRICE_LESS_THAN_ZERO])
+                             [TestData.BUN_PRICE_VALID, TestData.BUN_PRICE_FLOAT])
     def test_get_price_successful(self, price):
         ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_SAUCE, TestData.SAUCE_NAME_CHILI, price)
         assert ingredient.get_price() == price
 
-    @pytest.mark.parametrize('ingredient_name',
-                             [TestData.SAUCE_NAME_CHILI, TestData.SAUCE_NAME_HOT, TestData.SAUCE_NAME_SOUR])
-    def test_get_name_of_sauce_successful(self, ingredient_name):
-        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_SAUCE, ingredient_name, TestData.BUN_PRICE_VALID)
-        assert ingredient.get_name() == ingredient_name
+    def test_get_price_unsuccessful(self):
+        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_SAUCE, TestData.SAUCE_NAME_CHILI, TestData.BUN_PRICE_LESS_THAN_ZERO)
+        assert ingredient.get_price() != TestData.BUN_PRICE_LESS_THAN_ZERO
 
-    @pytest.mark.parametrize('ingredient_name',
-                             [TestData.FILLING_NAME_SAUSAGE, TestData.FILLING_NAME_DINOSAUR,
-                              TestData.FILLING_NAME_CUTLET])
-    def test_get_name_of_filling_successful(self, ingredient_name):
-        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_FILLING, ingredient_name, TestData.BUN_PRICE_VALID)
-        assert ingredient.get_name() == ingredient_name
+    def test_get_name_of_sauce_successful(self):
+        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_SAUCE, TestData.SAUCE_NAME_CHILI, TestData.BUN_PRICE_VALID)
+        assert ingredient.get_name() == TestData.SAUCE_NAME_CHILI
+
+    def test_get_name_of_filling_successful(self):
+        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_FILLING, TestData.FILLING_NAME_SAUSAGE, TestData.BUN_PRICE_VALID)
+        assert ingredient.get_name() == TestData.FILLING_NAME_SAUSAGE
 
     @pytest.mark.parametrize('ingredient_type',
                              [ingredient_types.INGREDIENT_TYPE_SAUCE, ingredient_types.INGREDIENT_TYPE_FILLING])
