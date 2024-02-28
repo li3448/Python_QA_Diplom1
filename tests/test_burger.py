92ae50b4387868d8e3f9821a56aefc4471925b3a
Yuliya Bolotskikh <bolotskikh.jul@yandex.ru>
2024-02-28 19:46:32 +0700

    new commit

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -2,7 +2,6 @@ from unittest.mock import Mock
 from praktikum.burger import Burger
 from praktikum.ingredient import Ingredient
 from praktikum.bun import Bun
-from praktikum.database import Database
 
 
 class TestBurger:
@@ -16,16 +15,16 @@ class TestBurger:
 
     def test_add_ingredients(self):
         burger = Burger()
-        ingredient = Ingredient('ingredient_type', 'name', 100.00)
+        ingredient = Ingredient('ingredient_type', 'name', 100.0)
         burger.add_ingredient(ingredient)
 
         assert burger.ingredients[0] == ingredient
 
     def test_remove_ingredient(self):
         burger = Burger()
-        ingredient_1 = Ingredient('ingredient_type', 'sauce', 100.00)
+        ingredient_1 = Ingredient('ingredient_type', 'sauce', 100.0)
         burger.add_ingredient(ingredient_1)
-        ingredient_2 = Ingredient('ingredient_type', 'filling', 200.00)
+        ingredient_2 = Ingredient('ingredient_type', 'filling', 200.0)
         burger.add_ingredient(ingredient_2)
         burger.remove_ingredient(1)
 
@@ -33,35 +32,39 @@ class TestBurger:
 
     def test_move_ingredient(self):
         burger = Burger()
-        ingredient_1 = Ingredient('ingredient_type', 'sauce', 100.00)
+        ingredient_1 = Ingredient('ingredient_type', 'sauce', 100.0)
         burger.add_ingredient(ingredient_1)
-        ingredient_2 = Ingredient('ingredient_type', 'filling', 200.00)
+        ingredient_2 = Ingredient('ingredient_type', 'filling', 200.0)
         burger.add_ingredient(ingredient_2)
         burger.move_ingredient(1, 0)
 
         assert burger.ingredients == [ingredient_2, ingredient_1]
 
     def test_get_price(self):
-        mock_bun = Mock(spec=Bun)
-        mock_bun.get_price.return_value = 200.00
-        mock_ingredient = Mock(spec=Ingredient)
-        mock_ingredient.get_price.return_value = 300.00
+        bun_mock = Mock(spec=Bun)
+        ingredient_mock = Mock(spec=Ingredient)
+        bun_mock.get_price.return_value = 300.0
+        ingredient_mock.get_price.return_value = 200.0
         burger = Burger()
+        burger.bun = bun_mock
+        burger.ingredients = [ingredient_mock]
 
-        assert burger.get_price() == 700.00
+        assert burger.get_price() == 800.0
 
-    def test_get_receipt(self):
-        mock_bun = Mock(spec=Bun)
-        mock_bun.get_name.return_value = 'red bun'
-        mock_bun.get_price.return_value = 300.00
-        mock_ingredient = Mock(spec=Ingredient)
-        mock_ingredient.get_name.return_value = 'dinosaur'
-        mock_ingredient.get_type.return_value = 'FILLING'
-        mock_ingredient.get_price.return_value = 200.00
+    def test_get_receipt_(self):
+        bun_mock = Mock(spec=Bun)
+        ingredient_mock = Mock(spec=Ingredient)
+        bun_mock.get_name.return_value = 'red bun'
+        ingredient_mock.get_type.return_value = 'sauce'
+        ingredient_mock.get_name.return_value = 'sour cream'
+        bun_mock.get_price.return_value = 300.0
+        ingredient_mock.get_price.return_value = 200.0
         burger = Burger()
+        burger.bun = bun_mock
+        burger.ingredients = [ingredient_mock]
 
-        assert burger.get_receipt() == [f'(==== red bun ====)', f'= filling dinosaur =',
-                                        f'(==== red bun ====)\n', f'Price: 800.00 ']
+        assert burger.get_receipt() == '(==== red bun ====)\n= sauce sour cream =\n(==== red bun ====)\n\nPrice: 800.0'
+
 
 
 
--- tests/test_database.py
+++ tests/test_database.py
@@ -14,18 +14,18 @@ class TestDatabase:
 
         assert len(data.available_ingredients()) == 6
 
-    @pytest.mark.parametrize("bun, price", [["black bun", 100.00],
-                                            ["white bun", 200.00],
-                                            ["red bun", 300.00]])
+    @pytest.mark.parametrize("bun, price", [["black bun", 100.0],
+                                            ["white bun", 200.0],
+                                            ["red bun", 300.0]])
     def test_available_buns_lst(self, bun: str, price: float):
         data = Database()
         data.buns = (bun, price)
 
         assert data.available_buns() == data.buns
 
-    @pytest.mark.parametrize("type_ingredient, ingredient, price", [["filling", "cutlet", 100.00],
-                                                                    ["sauce",  "hot sauce", 100.00],
-                                                                    ["filling", "sausage", 300.00]])
+    @pytest.mark.parametrize("type_ingredient, ingredient, price", [["filling", "cutlet", 100.0],
+                                                                    ["sauce",  "hot sauce", 100.0],
+                                                                    ["filling", "sausage", 300.0]])
     def test_available_ingredients_lst(self, type_ingredient, ingredient, price):
         data = Database()
         data.ingredients = (type_ingredient, ingredient, price)
