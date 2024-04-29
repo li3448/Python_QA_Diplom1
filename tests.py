7d06123e59c7d08cea9a9db6831ab44dd0ed4928
Andrei Maksimov <a.maksimov@brandquad.ru>
2024-04-29 16:20:48 +0400

    covered class Burger

--- tests.py
+++ tests.py
@@ -92,38 +92,84 @@ class TestBurger:
         assert burger.ingredients[0].get_price() == database_ingredient[0].get_price()
 
     @patch('Diplom_1.ingredient.Ingredient')
-    @patch('Diplom_1.ingredient.Ingredient')
     @patch('Diplom_1.bun.Bun')
-    def test_get_price(self, mock_ingredient_sause, mock_ingredient_filling, mock_bun):
+    @pytest.mark.parametrize('index_bun, index_sause, index_filling',
+                             [
+                                 (0, 0, 3)
+                             ])
+    def test_get_price(self, mock_ingredient, mock_bun, index_bun, index_sause, index_filling):
         burger = Burger()
         database = Database()
         database_ingredient = database.ingredients
 
-        mock_ingredient_sause = mock_ingredient_sause.return_value
-        mock_ingredient_sause.get_type.return_value = database_ingredient[0].get_type()
-        mock_ingredient_sause.get_name.return_value = database_ingredient[0].get_name()
-        mock_ingredient_sause.get_price.return_value = database_ingredient[0].get_price()
-
-        burger.add_ingredient(mock_ingredient_sause)
-
-        mock_ingredient_filling = mock_ingredient_filling.return_value
-        mock_ingredient_filling.get_type.return_value = database_ingredient[3].get_type()
-        mock_ingredient_filling.get_name.return_value = database_ingredient[3].get_name()
-        mock_ingredient_filling.get_price.return_value = database_ingredient[3].get_price()
-
-        burger.add_ingredient(mock_ingredient_filling)
-
-
         mock_bun = mock_bun.return_value
-        mock_bun.get_name.return_value = database.buns[0].get_name()
-        mock_bun.get_price.return_value = database.buns[0].get_price()
+        mock_bun.get_name.return_value = database.buns[index_bun].get_name()
+        mock_bun.get_price.return_value = database.buns[index_bun].get_price()
 
         burger.set_buns(mock_bun)
+
+
+        mock_ingredient = mock_ingredient.return_value
+        mock_ingredient.get_type.return_value = database_ingredient[index_sause].get_type()
+        mock_ingredient.get_name.return_value = database_ingredient[index_sause].get_name()
+        mock_ingredient.get_price.return_value = database_ingredient[index_sause].get_price()
+
+        burger.add_ingredient(mock_ingredient)
+
+        mock_ingredient = mock_ingredient.return_value
+        mock_ingredient.get_type.return_value = database_ingredient[index_filling].get_type()
+        mock_ingredient.get_name.return_value = database_ingredient[index_filling].get_name()
+        mock_ingredient.get_price.return_value = database_ingredient[index_filling].get_price()
+
+        burger.add_ingredient(mock_ingredient)
 
 
         price = burger.get_price()
 
         assert price == 400
+
+    @patch('Diplom_1.ingredient.Ingredient')
+    @patch('Diplom_1.bun.Bun')
+    @pytest.mark.parametrize('index_bun, index_sauce, index_filling',
+                             [
+                                 (0, 0, 4)
+                             ])
+    def test_get_receipt(self, mock_ingredient, mock_bun,  index_bun, index_sauce, index_filling):
+        burger = Burger()
+        database = Database()
+        database_ingredient = database.ingredients
+
+        mock_bun = mock_bun.return_value
+        mock_bun.get_name.return_value = database.buns[index_bun].get_name()
+        mock_bun.get_price.return_value = database.buns[index_bun].get_price()
+
+        burger.set_buns(mock_bun)
+
+        mock_ingredient = mock_ingredient.return_value
+        mock_ingredient.get_type.return_value = database_ingredient[index_sauce].get_type()
+        mock_ingredient.get_name.return_value = database_ingredient[index_sauce].get_name()
+        mock_ingredient.get_price.return_value = database_ingredient[index_sauce].get_price()
+
+        burger.add_ingredient(mock_ingredient)
+
+        mock_ingredient = mock_ingredient.return_value
+        mock_ingredient.get_type.return_value = database_ingredient[index_filling].get_type()
+        mock_ingredient.get_name.return_value = database_ingredient[index_filling].get_name()
+        mock_ingredient.get_price.return_value = database_ingredient[index_filling].get_price()
+
+        burger.add_ingredient(mock_ingredient)
+
+
+        receipt = burger.get_receipt()
+
+        print(receipt)
+
+        assert receipt.count(mock_bun.get_name()) == 2
+        assert "sauce" in receipt
+        assert "hot sauce" in receipt
+        assert "filling" in receipt
+        assert "dinosaur" in receipt
+        assert "Price: 500" in receipt
 
 
 
