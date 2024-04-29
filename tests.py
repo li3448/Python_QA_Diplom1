e07109437179be82416c4b92a51cf947b5bae33f
Andrei Maksimov <a.maksimov@brandquad.ru>
2024-04-29 17:49:03 +0400

    covered class Ingredient

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from unittest.mock import patch, Mock
 import pytest
 from bun import Bun
 from burger import Burger
+from ingredient import Ingredient
 from database import Database
 
 class TestBun:
@@ -173,8 +174,41 @@ class TestBurger:
 
 
 
+class TestIngredient:
+
+    @pytest.mark.parametrize('index', [0, 1, 2, 3, 4, 5])
+    def test_get_price(self, index):
+        database = Database()
+        type = database.ingredients[index].get_type()
+        name = database.ingredients[index].get_name()
+        price = database.ingredients[index].get_price()
+
+        ingredients = Ingredient(type, name, price)
+
+        assert ingredients.get_price() == price
+
+    @pytest.mark.parametrize('index', [0, 1, 2, 3, 4, 5])
+    def test_get_name(self, index):
+        database = Database()
+        type = database.ingredients[index].get_type()
+        name = database.ingredients[index].get_name()
+        price = database.ingredients[index].get_price()
+
+        ingredients = Ingredient(type, name, price)
+
+        assert ingredients.get_name() == name
 
 
+    @pytest.mark.parametrize('index', [0, 1, 2, 3, 4, 5])
+    def test_get_type(self, index):
+        database = Database()
+        type = database.ingredients[index].get_type()
+        name = database.ingredients[index].get_name()
+        price = database.ingredients[index].get_price()
+
+        ingredients = Ingredient(type, name, price)
+
+        assert ingredients.get_type() == type
 
 
 
