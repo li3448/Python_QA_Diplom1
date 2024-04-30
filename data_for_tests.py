3780b4bb987d12866c7b9193f9ae22adf67b2871
Artem Akilov <bfvniiaes-aas@yandex.ru>
2024-04-30 21:12:54 +0500

    Commit_001

---
+++ data_for_tests.py
@@ -0,0 +1,10 @@
+# данные для проверки bun.py
+
+name_of_bun = 'Вкусная'
+price_of_bun = 13.23
+
+# данные для проверки ingredient.py
+
+type_of_ingredient = 'SAUCE'
+name_of_ingredient = 'СвитЧили'
+price_of_ingredient = 8.13
