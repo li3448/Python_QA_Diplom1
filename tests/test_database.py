a88de9389b815718b84d240fd85f548a59c402d9
Yana Karpova <yanakarpova@hotmail.com>
2024-05-07 20:15:35 +0300

    Diplom_1: corrected test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,12 +2,12 @@ from praktikum.database import Database
 
 
 class TestDatabase:
-    def test_available_buns():
+    def test_available_buns(self):
         data_base = Database()
         buns = data_base.available_buns()
         assert len(buns) > 0
 
-    def test_available_ingredients():
+    def test_available_ingredients(self):
         data_base = Database()
         ingredients = data_base.available_ingredients()
         assert len(ingredients) > 0
