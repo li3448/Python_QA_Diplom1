5777f42b9906e0064ab29809cc4cf9a950972e09
Elena Sizova <gredhen77@gmail.com>
2024-05-22 01:30:51 +0300

    .

--- helper.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def get_random_bun(dbs):
-    return random.choice(dbs.available_buns())
-
-
-def get_random_ingredient(dbs):
-    return random.choice(dbs.available_ingredients())
--- tests/test_bun.py
+++
@@ -1,6 +0,0 @@
-class TestBun:
-    def test_bun_name_price(self, bun):
-        name = bun.name
-        price = bun.price
-        assert bun.get_name() == name
-        assert bun.get_price() == price
