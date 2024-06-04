c8fde65c238c6f927e89afcd2f59af66a656a8f1
aleckseygrishin <combine696@yandex.ru>
2024-06-04 03:28:21 +0300

    edit conftests file. delete covhtml from .gitignore

--- conftest.py
+++ conftest.py
@@ -1,18 +1,38 @@
 from unittest.mock import Mock
 from praktikum.bun import Bun
+from praktikum.database import Database
 from praktikum.ingredient import Ingredient
+from praktikum.burger import Burger
 from data import Data
 
 import pytest
 
 
 @pytest.fixture(scope='function')
-def mock_bun():
-    bun = Mock()
-    bun.get_name.return_value = Data.BUN_NAME
-    bun.get_price.return_value = Data.BUN_PRICE
+def mock_bun_get_price():
+    mock_bun = Mock()
+    mock_bun.get_price.return_value = Data.BUN_PRICE
 
-    return bun
+    return mock_bun
+
+
+@pytest.fixture(scope='function')
+def mock_bun_get_name(mock_bun_get_price):
+    mock_bun_get_price.get_name.return_value = Data.BUN_NAME
+
+
+@pytest.fixture(scope='function')
+def mock_ingredient_get_price():
+    mock_ingredient = Mock()
+    mock_ingredient.get_price.return_value = Data.INGREDIENT_PRICE
+
+    return mock_ingredient
+
+
+@pytest.fixture(scope='function')
+def mock_ingredient_get_name_and_get_type(mock_ingredient_get_price):
+    mock_ingredient_get_price.get_name.return_value = Data.INGREDIENT_NAME
+    mock_ingredient_get_price.get_type.return_value = Data.INGREDIENTS_TYPE
 
 
 @pytest.fixture(scope='function')
@@ -27,3 +47,17 @@ def ingredient_instance():
     ingredient = Ingredient(name=Data.BUN_NAME, price=Data.BUN_PRICE, ingredient_type=Data.INGREDIENTS_TYPE)
 
     return ingredient
+
+
+@pytest.fixture(scope='function')
+def burger_instance():
+    burger = Burger()
+
+    return burger
+
+
+@pytest.fixture(scope='function')
+def database_instance():
+    database = Database()
+
+    return database
--- data.py
+++ data.py
@@ -1,4 +1,11 @@
 class Data:
     BUN_NAME = 'Burger'
     BUN_PRICE = 269.9
-    INGREDIENTS_TYPE = 'Bread'
+    INGREDIENT_PRICE = 800
+    INGREDIENTS_TYPE = 'Lite'
+    INGREDIENT_NAME = 'Bread'
+
+    RESULT_GET_RECEIPT_FROM_MOCK = (f'(==== {BUN_NAME} ====)'
+                                    f'\n= {INGREDIENTS_TYPE.lower()} {INGREDIENT_NAME} ='
+                                    f'\n(==== {BUN_NAME} ====)\n'
+                                    f'\nPrice: {BUN_PRICE * 2 + INGREDIENT_PRICE}')
