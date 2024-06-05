0c5d5e4e81d5864ed187fe423ce5bd2ec40597d7
Aleksey Maltsev <maltsevaleksey01@yandex.ru>
2024-06-05 22:16:04 +0300

    diplom1_1

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,26 +5,6 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE
 
 
 @pytest.fixture(scope='function')
-def burger():
-    burger = Burger()
-
-    mock_ingredient1 = Mock()
-    mock_ingredient1.type = INGREDIENT_TYPE_SAUCE
-    mock_ingredient1.name = 'Ingredient1'
-    mock_ingredient1.price = 100
-
-    mock_ingredient2 = Mock()
-    mock_ingredient2.type = INGREDIENT_TYPE_SAUCE
-    mock_ingredient2.name = 'Ingredient2'
-    mock_ingredient2.price = 100
-
-    burger.add_ingredient(mock_ingredient1)
-    burger.add_ingredient(mock_ingredient2)
-
-    return burger
-
-
-@pytest.fixture(scope='function')
 def mock_ingredient1():
     mock_ingredient1 = Mock()
     mock_ingredient1.type = INGREDIENT_TYPE_SAUCE
@@ -38,7 +18,7 @@ def mock_ingredient1():
 def mock_ingredient2():
     mock_ingredient2 = Mock()
     mock_ingredient2.type = INGREDIENT_TYPE_SAUCE
-    mock_ingredient2.name = 'Ingredient1'
+    mock_ingredient2.name = 'Ingredient2'
     mock_ingredient2.price = 100
 
     return mock_ingredient2
@@ -51,3 +31,11 @@ def mock_bun():
     mock_bun.price = 100
 
     return mock_bun
+
+
+@pytest.fixture(scope='function')
+def burger(mock_ingredient1, mock_ingredient2):
+    burger = Burger()
+    burger.add_ingredient(mock_ingredient1)
+    burger.add_ingredient(mock_ingredient2)
+    return burger
