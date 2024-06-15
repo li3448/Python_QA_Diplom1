387a513308a3ff1686e3c1aa1a59dbb6bd3183b5
zar <zarinasadykova.m@gmail.com>
2024-06-15 10:29:07 +0500

    fixed unit test

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -4,12 +4,21 @@ from praktikum.bun import Bun
 
 
 class TestBunParameterized(unittest.TestCase):
+
     @parameterized.expand([
         ("Булочка", 50.0),
         ("Черный хлеб", 70.0),
         ("Безглютеновая булочка", 100.0),
     ])
-    def test_bun_parameters(self, name, price):
+    def test_get_name(self, name, price):
         bun = Bun(name, price)
-        self.assertIs(bun.get_name(), name)
-        self.assertIs(bun.get_price(), price)
+        self.assertEqual(bun.get_name(), name)
+
+    @parameterized.expand([
+        ("Булочка", 50.0),
+        ("Черный хлеб", 70.0),
+        ("Безглютеновая булочка", 100.0),
+    ])
+    def test_get_price(self, name, price):
+        bun = Bun(name, price)
+        self.assertEqual(bun.get_price(), price)
